338cc6107949feb41643f1404a3ed658e0b521ce
paslin <agentsuperone7@gmail.com>
2026-03-04 15:23:57 +0530

    Increase body size limit

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
